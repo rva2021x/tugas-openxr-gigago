6d0c585156c4f15c342f99b3834c11f7e2b26a19
LarV123 <geizka.fahriza@gmail.com>
2021-11-23 16:50:35 +0700

    basic push and pull ui component

---
+++ Assets/Scripts/UI/UITextPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace UI {
+	public class UITextPanel : MonoBehaviour {
+
+		[SerializeField] private TextMeshProUGUI text;
+
+		public void Show(string str) {
+			text.text = str;
+			gameObject.SetActive(true);
+		}
+
+		public void Hide() {
+			gameObject.SetActive(false);
+		}
+
+	}
+}
