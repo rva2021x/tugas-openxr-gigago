afef0c94990615da029c4ccb563b9d5917601cfc
IktaS <imamrafiia@gmail.com>
2021-12-14 20:10:15 +0700

    add big ass timer

---
+++ Assets/Scripts/UI/UITimer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{
+    [SerializeField] private Text TimerText;
+    private bool playing;
+    private float Timer;
+
+    private void Start()
+    {
+        Timer = 0;
+        playing = false;
+    }
+
+    void Update()
+    {
+
+        if (playing == true)
+        {
+
+            Timer += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(Timer / 60F);
+            int seconds = Mathf.FloorToInt(Timer % 60F);
+            int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+            TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+        }
+
+    }
+
+    public void Pause()
+    {
+        playing = false;
+    }
+
+    public void ResetTimer()
+    {
+        Timer = 0;
+    }
+
+    public void Stop()
+    {
+        Pause();
+        ResetTimer();
+    }
+
+    public void StartTimer()
+    {
+        playing = true;
+    }
+}
